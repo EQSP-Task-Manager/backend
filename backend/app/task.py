b2da20ddcb4434e743001dfc26a76ff9a74094e3
Maxim Pryanikov <mx.pryanikov@gmail.com>
2022-12-25 04:31:40 +0300

    Implemented other task methods

--- backend/app/task.py
+++ backend/app/task.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 
 from backend import interfaces
 from backend.models import Task
-from .errors import NonExistentTaskIDError
+from .errors import OutdatedRevisionError, NonExistentTaskIDError
 
 
 class TaskService(interfaces.TaskService):
@@ -27,3 +27,42 @@ class TaskService(interfaces.TaskService):
             if task is None:
                 raise NonExistentTaskIDError(task_id)
         return task, curr_revision
+
+    async def add_tasks(self, user_id: str, tasks: list[Task], revision: int) -> int:
+        async with self._engine.begin() as conn:
+            curr_revision = await self._repo.get_revision(conn, user_id)
+            if curr_revision is None:
+                curr_revision = 0
+                await self._repo.set_init_revision(conn, user_id)
+            if revision < curr_revision:
+                raise OutdatedRevisionError(revision, curr_revision)
+            await self._repo.add_tasks(conn, user_id, tasks)
+            await self._repo.increment_revision(conn, user_id)
+        return curr_revision + 1
+
+    async def delete_task(self, user_id: str, task_id: UUID4) -> int:
+        async with self._engine.begin() as conn:
+            curr_revision = await self._repo.get_revision(conn, user_id)
+            await self._repo.delete_task(conn, user_id, task_id)
+            await self._repo.increment_revision(conn, user_id)
+        return curr_revision + 1
+
+    async def update_task(self, user_id: str, task: Task):
+        async with self._engine.begin() as conn:
+            curr_revision = await self._repo.get_revision(conn, user_id)
+            await self._repo.update_task(conn, user_id, task)
+            await self._repo.increment_revision(conn, user_id)
+        return curr_revision + 1
+
+    async def update_tasks(self, user_id: str, tasks: list[Task], revision: int):
+        async with self._engine.begin() as conn:
+            curr_revision = await self._repo.get_revision(conn, user_id)
+            if curr_revision is None:
+                curr_revision = 0
+                await self._repo.set_init_revision(conn, user_id)
+            if revision < curr_revision:
+                raise OutdatedRevisionError(revision, curr_revision)
+            await self._repo.delete_tasks(conn, user_id)
+            await self._repo.add_tasks(conn, user_id, tasks)
+            await self._repo.increment_revision(conn, user_id)
+        return curr_revision + 1
